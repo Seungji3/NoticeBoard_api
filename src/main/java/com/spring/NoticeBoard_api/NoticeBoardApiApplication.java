278eb7e70670fbf40b70fe8e554c5a81b05b7b91
user01\User <a01066018235@gmail.com>
2023-11-27 20:47:05 +0900

    feat: 프로젝트 색성

---
+++ src/main/java/com/spring/NoticeBoard_api/NoticeBoardApiApplication.java
@@ -0,0 +1,13 @@
+package com.spring.NoticeBoard_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeBoardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticeBoardApiApplication.class, args);
+	}
+
+}
